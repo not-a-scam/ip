8948ab3892d1f7d792261d961e5802c991e13d90
Zechariah Yeo <70231898+not-a-scam@users.noreply.github.com>
2025-08-18 14:49:08 +0800

    Rename Duke to Nomz

---
+++ src/main/java/Nomz.java
@@ -0,0 +1,6 @@
+public class Nomz {
+    public static void main(String[] args) {
+        String lineBreak = "____________________________________";
+        System.out.println(lineBreak + "\nHi im nomz! \nhope you're having a nomztacular day\n" + lineBreak);
+    }
+}
