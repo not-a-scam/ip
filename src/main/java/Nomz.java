7d65dadd43cc695a89f9887ae8dc132a373e2647
Zechariah Yeo <70231898+not-a-scam@users.noreply.github.com>
2025-08-18 14:50:26 +0800

    Add exit greeting

--- src/main/java/Nomz.java
+++ src/main/java/Nomz.java
@@ -2,5 +2,6 @@ public class Nomz {
     public static void main(String[] args) {
         String lineBreak = "____________________________________";
         System.out.println(lineBreak + "\nHi im nomz! \nhope you're having a nomztacular day\n" + lineBreak);
+        System.out.println("Bye! hope to see you again soon!");
     }
 }
