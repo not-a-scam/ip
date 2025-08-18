bc0c49ecccc961bad26a9bc13f2aa6482fd76268
Zechariah Yeo <70231898+not-a-scam@users.noreply.github.com>
2025-08-18 15:26:03 +0800

    Add chat logic to Nomz.java

--- src/main/java/Nomz.java
+++ src/main/java/Nomz.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Nomz {
     public static final String LINEBREAK = "-----------------------------------------";
     public static final String BYE = "Bye! hope to see you again soon!" ; 
@@ -25,5 +27,15 @@ public class Nomz {
     public static void main(String[] args) {
         // Greeting
         System.out.println(responseFormat("Hi im nomz! \nhope you're having a nomztacular day"));        
+
+        // Chat
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+        while(!input.equals("bye")) {
+            System.out.println(echo(input));
+            input = sc.nextLine();
+        }
+        System.out.println(responseFormat(BYE));
+        sc.close();
     }
 }
