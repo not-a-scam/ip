eb76e58768947f52d282b782dd854f077e7cf7cb
Zechariah Yeo <70231898+not-a-scam@users.noreply.github.com>
2025-08-18 15:11:03 +0800

    Add responseFormat function
    A helper method to format responses from the chatbot

--- src/main/java/Nomz.java
+++ src/main/java/Nomz.java
@@ -1,8 +1,19 @@
 public class Nomz {
-    public static final String LINEBREAK = "____________________________________";
+    public static final String LINEBREAK = "-----------------------------------------";
     public static final String BYE = "Bye! hope to see you again soon!" ; 
+
+    /**
+     * Formats a given string to be printed as a response from the chatbot
+     * 
+     * @param input String to be formatted
+     * @return Formatted string 
+     */
+    public static String responseFormat(String input) {
+        return LINEBREAK + "\n" + input + "\n" + LINEBREAK;
+    }
+
     public static void main(String[] args) {
-        System.out.println(LINEBREAK + "\nHi im nomz! \nhope you're having a nomztacular day\n" + LINEBREAK);
-        System.out.println(BYE);
+        System.out.println(responseFormat("Hi im nomz! \nhope you're having a nomztacular day"));
+        System.out.println(responseFormat(BYE));
     }
 }
