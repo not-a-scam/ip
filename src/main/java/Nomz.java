72594ca46991ed787884ea0d5f63a19bfbd04589
Zechariah Yeo <70231898+not-a-scam@users.noreply.github.com>
2025-08-18 15:05:14 +0800

    Make exit and linebreak texts into finals

--- src/main/java/Nomz.java
+++ src/main/java/Nomz.java
@@ -1,7 +1,8 @@
 public class Nomz {
+    public static final String LINEBREAK = "____________________________________";
+    public static final String BYE = "Bye! hope to see you again soon!" ; 
     public static void main(String[] args) {
-        String lineBreak = "____________________________________";
-        System.out.println(lineBreak + "\nHi im nomz! \nhope you're having a nomztacular day\n" + lineBreak);
-        System.out.println("Bye! hope to see you again soon!");
+        System.out.println(LINEBREAK + "\nHi im nomz! \nhope you're having a nomztacular day\n" + LINEBREAK);
+        System.out.println(BYE);
     }
 }
