7c9edfcb3639788e85149c9920846fe9a5418faf
Zechariah Yeo <70231898+not-a-scam@users.noreply.github.com>
2025-08-18 16:19:47 +0800

    Update toString method in Task.java

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -18,5 +18,10 @@ public class Task {
     public void unmark() {
         this.isDone = false;
     }
+
+    @Override
+    public String toString(){
+        return "[" + getStatusIcon() + "] " + description;  
+    }
 }
 
