65d01a35e739ae6d4fb85bf83c1e2899f2a78503
Zechariah Yeo <70231898+not-a-scam@users.noreply.github.com>
2025-08-27 00:52:35 +0800

    Update AddDeadlineCommand.java

--- src/main/java/AddDeadlineCommand.java
+++ src/main/java/AddDeadlineCommand.java
@@ -3,15 +3,33 @@ import java.time.LocalDateTime;
 
 public class AddDeadlineCommand extends Command {
     private final String description;
-    private final LocalDateTime by;
-    public AddDeadlineCommand(String description, LocalDateTime by) {
+    private final LocalDateTime byTime;
+    private final String by;
+    private final boolean useDateTime;
+
+    public AddDeadlineCommand(String description, LocalDateTime byTime) {
         this.description = description;
+        this.byTime = byTime;
+        this.by = null;
+        useDateTime = true;
+    }
+
+    public AddDeadlineCommand(String description, String by) {
+        this.description = description;
+        this.byTime = null;
         this.by = by;
+        useDateTime = false;
     }
 
     @Override
     public void execute(TaskList tasks, Ui ui, Storage storage) {
-        Task t = tasks.add(new Deadline(description, by));
+        Task t;
+        if(!useDateTime) {
+            t = new Deadline(description, by);
+        } else {
+            t = new Deadline(description, byTime);
+        }
+        tasks.add(t);
         try { 
             storage.append(t); 
         } catch (IOException e) { 
