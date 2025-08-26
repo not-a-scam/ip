295421452c0ebe855ae562c8c5ece2e4f32da5f4
Zechariah Yeo <70231898+not-a-scam@users.noreply.github.com>
2025-08-26 12:32:18 +0800

    Add enum for Tasks

--- src/main/java/Todo.java
+++ src/main/java/Todo.java
@@ -1,12 +1,7 @@
 public class Todo extends Task {
     
     public Todo(String description){
-        super(description);
+        super(description, TaskType.TODO);
 
-    }
-
-    @Override
-    public String toString(){
-        return "[T]" + super.toString();
     }
 }
