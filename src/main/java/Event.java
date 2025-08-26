c1c8417b7c13ff979f6fd1db15db15c25d986b6c
Zechariah Yeo <70231898+not-a-scam@users.noreply.github.com>
2025-08-26 15:25:33 +0800

    Update Event.java

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,21 +1,44 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Event extends Task{
+
+    private static final DateTimeFormatter OUT = DateTimeFormatter.ofPattern("MMM dd yyyy, h:mma");
 
     private String from;
     private String to;
+    private LocalDateTime fromTime = null;
+    private LocalDateTime toTime = null;
+
+    private Event(String description) {
+        super(description, TaskType.EVENT);
+    }
 
     public Event(String description, String from, String to) {
-        super(description, TaskType.EVENT);
+        this(description);
         this.from = from;
         this.to = to;
     }
 
+    public Event(String description, LocalDateTime fromTime, LocalDateTime toTime) {
+        this(description);
+        this.fromTime = fromTime;
+        this.toTime = toTime;
+    }
+
     @Override
     public String toString() {
-        return super.toString() + " (from: " + from + " to: " + to + ")";
+        if (fromTime == null || toTime == null) {
+            return super.toString() + " (from: " + from + " to: " + to + ")";
+        }
+        return super.toString() + " (from: " + OUT.format(fromTime) + " to: " + OUT.format(toTime) + ")";
     }
 
     @Override
     public String savedString(){
-        return super.savedString() + "|" + from + "|" + to;
+        if (fromTime == null || toTime == null) {
+            return super.savedString() + "|" + from + "|" + to;
+        }
+        return super.savedString() + "|" + fromTime.toString() + "|" + toTime.toString();
     }
 }
