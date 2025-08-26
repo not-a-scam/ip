650596a09d63c46b8d5fac928c808a87819b7a5c
Zechariah Yeo <70231898+not-a-scam@users.noreply.github.com>
2025-08-26 15:15:09 +0800

    Update Deadline.java

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,19 +1,35 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Deadline extends Task {
 
-    protected String by;
+    protected LocalDateTime by = null;
+    protected String byDescription;
+    private static final DateTimeFormatter OUT = DateTimeFormatter.ofPattern("MMM dd yyyy, h:mma");
 
-    public Deadline(String description, String by) {
+    public Deadline(String description, LocalDateTime by) {
         super(description, TaskType.DEADLINE);
         this.by = by;
     }
 
+    public Deadline(String description, String byDescription) {
+        this(description, (LocalDateTime) null);
+        this.byDescription = byDescription;  
+    }
+
     @Override
     public String toString() {
-        return super.toString() + " (by: " + by + ")";
+        if(by.equals(null)) {
+            return super.toString() + " (by: " + byDescription + ")";
+        }
+        return super.toString() + " (by: " + OUT.format(by) + ")";
     }
 
     @Override
     public String savedString(){
-        return super.savedString() + "|" + by;
+        if(by.equals(null)) {
+            return super.savedString() + "|" + byDescription;
+        }
+        return super.savedString() + "|" + by.toString();
     }
 }
