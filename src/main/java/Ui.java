1acb81df368993101eb7755aa9eea3b0ebae0ecc
Zechariah Yeo <70231898+not-a-scam@users.noreply.github.com>
2025-08-26 17:00:39 +0800

    Add Ui.java

---
+++ src/main/java/Ui.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Ui {
+
+    private final Scanner scanner = new Scanner(System.in);
+
+    public void show(String s) {
+        System.out.println(Messages.MESSAGE_LINEBREAK + "\n" + s + "\n" + Messages.MESSAGE_LINEBREAK);
+    }
+    public void showWelcome() {
+        show(Messages.MESSAGE_WELCOME);
+    }
+
+    public String readCommand() {
+        return scanner.nextLine();
+    }
+
+    public void showError(String message) {
+        System.err.println(Messages.MESSAGE_LINEBREAK + "\n" + message + "\n" + Messages.MESSAGE_LINEBREAK);
+    }
+
+    public void showLoaded() {
+        show(Messages.MESSAGE_LOAD_TASK_SUCCESS);
+    }
+
+    public void showGoodbye() {
+        show(Messages.MESSAGE_BYE);
+        scanner.close();
+    }
+}
