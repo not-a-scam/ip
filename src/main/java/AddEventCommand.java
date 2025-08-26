fa06b4bcdc5fc666c1a0177b4670dacb0ed30558
Zechariah Yeo <70231898+not-a-scam@users.noreply.github.com>
2025-08-27 00:50:22 +0800

    Update AddEventCommand.java

--- src/main/java/AddEventCommand.java
+++ src/main/java/AddEventCommand.java
@@ -4,16 +4,37 @@ import java.time.LocalDateTime;
 
 public class AddEventCommand extends Command {
     private final String description;
-    private final LocalDateTime from, to;
-    public AddEventCommand(String description, LocalDateTime from, LocalDateTime to) {
+    private final LocalDateTime fromTime, toTime;
+    private final String from, to;
+    private final boolean useDateTime;
+
+    public AddEventCommand(String description, LocalDateTime fromTime, LocalDateTime toTime) {
         this.description = description; 
-        this.from = from; 
+        this.fromTime = fromTime; 
+        this.toTime = toTime;
+        this.from = null;
+        this.to = null;
+        this.useDateTime = true;
+    }
+
+    public AddEventCommand(String description, String from, String to) {
+        this.description = description; 
+        this.fromTime = null; 
+        this.toTime = null;
+        this.from = from;
         this.to = to;
+        useDateTime = false;
     }
 
     @Override
     public void execute(TaskList tasks, Ui ui, Storage storage) {
-        Task t = tasks.add(new Event(description, from, to));
+        Task t;
+        if(!useDateTime) {
+            t = new Event(description, from, to);
+        } else {
+            t = new Event(description, fromTime, toTime);
+        }
+        tasks.add(t);
         try { 
             storage.append(t); 
         } catch (IOException e) { 
