ee0235b757dfbc9f1043356c296895411d568b5f
Zechariah Yeo <70231898+not-a-scam@users.noreply.github.com>
2025-08-27 00:44:17 +0800

    Add MarkCommand.java

---
+++ src/main/java/MarkCommand.java
@@ -0,0 +1,22 @@
+public class MarkCommand extends Command {
+    private final int index;
+    private final boolean toMark;
+    
+    public MarkCommand(int index, boolean toMark) { 
+        this.index = index; 
+        this.toMark = toMark; 
+    }
+
+    @Override
+    public void execute(TaskList tasks, Ui ui, Storage storage) throws NomzException {
+        Task t = tasks.get(index);
+        if (toMark) {
+            t.mark();
+            ui.show(Messages.MESSAGE_TASK_MARKED.formatted(t));
+        } else {
+            t.unmark();
+            ui.show(Messages.MESSAGE_TASK_UNMARKED.formatted(t));
+        }
+        try { storage.saveAll(tasks.getTasks()); } catch (Exception e) { ui.showError(e.getMessage()); }
+    }
+}
