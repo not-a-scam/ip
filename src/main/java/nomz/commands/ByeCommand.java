9148dfe6b1706f9ac74f4b125ca23bed16da4c4d
Zechariah Yeo <70231898+not-a-scam@users.noreply.github.com>
2025-08-27 23:21:56 +0800

    ByeCommand.java: Add class header comment

--- src/main/java/nomz/commands/ByeCommand.java
+++ src/main/java/nomz/commands/ByeCommand.java
@@ -4,6 +4,9 @@ import nomz.data.tasks.TaskList;
 import nomz.storage.Storage;
 import nomz.ui.Ui;
 
+/**
+ * Command to exit the application.
+ */
 public class ByeCommand extends Command {
     @Override
     public void execute(TaskList tasks, Ui ui, Storage storage) {
