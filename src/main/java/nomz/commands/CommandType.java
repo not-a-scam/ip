0b56bd350de521529844a0d8444c2a27f10ac01e
Zechariah Yeo <70231898+not-a-scam@users.noreply.github.com>
2025-08-27 23:20:23 +0800

    CommandType.java: Add JavaDoc comments

--- src/main/java/nomz/commands/CommandType.java
+++ src/main/java/nomz/commands/CommandType.java
@@ -4,6 +4,12 @@ import nomz.data.exception.InvalidNomzCommandException;
 public enum CommandType {
     LIST, MARK, UNMARK, TODO, DEADLINE, EVENT, DELETE, BYE;
 
+    /**
+     * Converts a string representation of a command to its CommandType enum.
+     * @param input
+     * @return
+     * @throws InvalidNomzCommandException
+     */
     public static CommandType fromString(String input) throws InvalidNomzCommandException {
         try { 
             return CommandType.valueOf(input.toUpperCase()); 
