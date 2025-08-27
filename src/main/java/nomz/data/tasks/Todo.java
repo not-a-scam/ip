30f585a5c7f72a4b45bd6f51b58fca011e601416
Zechariah Yeo <70231898+not-a-scam@users.noreply.github.com>
2025-08-27 23:36:09 +0800

    Todo.java: Add JavaDoc comments

--- src/main/java/nomz/data/tasks/Todo.java
+++ src/main/java/nomz/data/tasks/Todo.java
@@ -1,7 +1,14 @@
 package nomz.data.tasks;
 
+/**
+ * Represents a todo task in Nomz.
+ */
 public class Todo extends Task {
     
+    /**
+     * Creates a todo task.
+     * @param description
+     */
     public Todo(String description){
         super(description, TaskType.TODO);
 
