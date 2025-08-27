5412a65c7cc8580a45d508ddc12dfe8d200337e2
Zechariah Yeo <70231898+not-a-scam@users.noreply.github.com>
2025-08-27 23:19:33 +0800

    Command.java: Add JavaDoc comments

--- src/main/java/nomz/commands/Command.java
+++ src/main/java/nomz/commands/Command.java
@@ -6,8 +6,20 @@ import nomz.storage.Storage;
 import nomz.ui.Ui;
 
 public abstract class Command {
+
+    /**
+     * Executes the command.
+     * @param tasks
+     * @param ui
+     * @param storage
+     * @throws NomzException
+     */
     public abstract void execute(TaskList tasks, Ui ui, Storage storage) throws NomzException;
 
+    /**
+     * Checks if the command is an exit command.
+     * @return
+     */
     public boolean isExit() { 
         return false; 
     }
