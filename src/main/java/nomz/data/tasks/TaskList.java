170183cc18e9b6f2707e89bba584c1d10da55179
Zechariah Yeo <70231898+not-a-scam@users.noreply.github.com>
2025-08-27 23:34:21 +0800

    TaskList.java: Add JavaDoc comments

--- src/main/java/nomz/data/tasks/TaskList.java
+++ src/main/java/nomz/data/tasks/TaskList.java
@@ -4,22 +4,43 @@ import java.util.ArrayList;
 import nomz.common.Messages;
 import nomz.data.exception.InvalidNomzArgumentException;
 
+/**
+ * Represents a list of tasks in Nomz.
+ */
 public class TaskList {
     private ArrayList<Task> tasks;
 
+    /**
+     * Creates a TaskList with an empty list of tasks.
+     */
     public TaskList() {
         this.tasks = new ArrayList<>();
     }
 
+    /**
+     * Creates a TaskList with the specified list of tasks.
+     * @param tasks
+     */
     public TaskList(ArrayList<Task> tasks) {
         this.tasks = new ArrayList<>(tasks);
     }
 
+    /**
+     * Adds a task to the list.
+     * @param task
+     * @return
+     */
     public Task add(Task task) {
         tasks.add(task);
         return task;
     }
 
+    /**
+     * Deletes a task from the list. Uses one based indexing.
+     * @param oneBasedIndex
+     * @return
+     * @throws InvalidNomzArgumentException
+     */
     public Task delete(int oneBasedIndex) throws InvalidNomzArgumentException {
         int idx = oneBasedIndex - 1;
         if (idx < 0 || idx >= tasks.size()) {
@@ -28,6 +49,12 @@ public class TaskList {
         return tasks.remove(idx);
     }
 
+    /**
+     * Retrieves a task from the list. Uses one based indexing.
+     * @param oneBasedIndex
+     * @return
+     * @throws InvalidNomzArgumentException
+     */
     public Task get(int oneBasedIndex) throws InvalidNomzArgumentException {
         int idx = oneBasedIndex - 1;
         if (idx < 0 || idx >= tasks.size()) {
@@ -36,14 +63,27 @@ public class TaskList {
         return tasks.get(idx);
     }
 
+    /**
+     * Returns the number of tasks in the list.
+     * @return
+     */
     public int size() {
         return tasks.size();
     }
 
+
+    /**
+     * Returns the list of tasks.
+     * @return
+     */
     public ArrayList<Task> getTasks() {
         return tasks;
     }
 
+    /**
+     * Returns a string representation of the task list for display.
+     * @return
+     */
     public String toDisplayString() {
         StringBuilder sb = new StringBuilder(Messages.MESSAGE_TASK_LIST_HEADER);
         for (int i = 0; i < tasks.size(); i++) {
