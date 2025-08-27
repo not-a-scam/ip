e064cc4887f61b894f1450300e32b8a89707c7d7
Zechariah Yeo <70231898+not-a-scam@users.noreply.github.com>
2025-08-27 23:38:57 +0800

    Storage.java: Add JavaDoc comments

--- src/main/java/nomz/storage/Storage.java
+++ src/main/java/nomz/storage/Storage.java
@@ -10,9 +10,14 @@ import nomz.data.exception.NomzException;
 import nomz.data.tasks.Task;
 import nomz.parser.Parser;
 
+/** Handles storage and retrieval of tasks. */
 public class Storage {
     private final File file;
     
+    /**
+     * Creates a Storage object to manage task storage.
+     * @param filePath The path to the file where tasks are stored.
+     */
     public Storage(String filePath) {
         this.file = new File(filePath);
         ensureFile();
@@ -30,7 +35,11 @@ public class Storage {
         }
     }
 
-    /** Load tasks from disk. */
+    /**
+     * Loads tasks from the storage file.
+     * @return A list of tasks loaded from the file.
+     * @throws NomzException If the file content is invalid.
+     */
     public ArrayList<Task> load() throws NomzException {
         ArrayList<Task> list = new ArrayList<>();
         try (Scanner s = new Scanner(file)) {
@@ -42,14 +51,22 @@ public class Storage {
         return list;
     }
 
-    /** Append a newly added task */
+    /**
+     * Appends a new task to the storage file.
+     * @param task The task to append.
+     * @throws IOException If an I/O error occurs.
+     */
     public void append(Task task) throws IOException {
         try (FileWriter fw = new FileWriter(file, true)) {
             fw.write(task.savedString() + "\n");
         }
     }
 
-    /** Overwrite the file with the current list (for mark/unmark/delete). */
+    /**
+     * Saves all tasks to the storage file.
+     * @param tasks The list of tasks to save.
+     * @throws IOException If an I/O error occurs.
+     */
     public void saveAll(ArrayList<Task> tasks) throws IOException {
         try (FileWriter fw = new FileWriter(file)) {
             for (Task t : tasks) {
