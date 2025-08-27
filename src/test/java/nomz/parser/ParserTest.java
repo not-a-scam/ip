2c4f992c163e957d22b77cd98cfbcbeeaaf13019
Zechariah Yeo <70231898+not-a-scam@users.noreply.github.com>
2025-08-27 21:56:36 +0800

    ParserTest.java: Add Todo test cases

--- src/test/java/nomz/parser/ParserTest.java
+++ src/test/java/nomz/parser/ParserTest.java
@@ -2,6 +2,12 @@ package nomz.parser;
 
 import nomz.commands.Command;
 import nomz.commands.AddTodoCommand;
+import nomz.commands.AddEventCommand;
+
+import java.beans.Transient;
+import java.time.LocalDateTime ;
+
+import static nomz.common.Messages.MESSAGE_NO_DESCRIPTION_ARGUMENT;
 
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -19,4 +25,42 @@ public class ParserTest {
         }
         assertEquals(expected, result);
     }
+
+    @Test
+    public void parse_validTodoWithExtraSpaces_returnsTodo() {
+        String input = "todo    read book";
+        Command expected = new AddTodoCommand("read book");
+        Command result = null;
+        try {
+            result = Parser.parse(input);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        assertEquals(expected, result);
+    }
+
+    @Test 
+    public void parse_validTodoWithTrailingSpaces_returnsTodo() {
+        String input = "todo read book    ";
+        Command expected = new AddTodoCommand("read book");
+        Command result = null;
+        try {
+            result = Parser.parse(input);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        assertEquals(expected, result);
+    }
+
+    @Test
+    public void parse_invalidTodoNoDescription_throwsException() {
+        String input = "todo";
+        Command result = null;
+        try {
+            result = Parser.parse(input);
+        } catch (Exception e) {
+            assertEquals(MESSAGE_NO_DESCRIPTION_ARGUMENT, e.getMessage());
+        }
+    }
+
 }
