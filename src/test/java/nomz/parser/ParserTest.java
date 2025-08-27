16611ed015772ea9970cad4c69b54db6e4ed4f78
Zechariah Yeo <70231898+not-a-scam@users.noreply.github.com>
2025-08-27 22:03:40 +0800

    ParserTest.java: Add Event test cases

--- src/test/java/nomz/parser/ParserTest.java
+++ src/test/java/nomz/parser/ParserTest.java
@@ -8,6 +8,8 @@ import java.beans.Transient;
 import java.time.LocalDateTime ;
 
 import static nomz.common.Messages.MESSAGE_NO_DESCRIPTION_ARGUMENT;
+import static nomz.common.Messages.MESSAGE_WRONG_FROM_KEYWORD;
+import static nomz.common.Messages.MESSAGE_WRONG_TO_KEYWORD;
 
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -60,6 +62,87 @@ public class ParserTest {
             result = Parser.parse(input);
         } catch (Exception e) {
             assertEquals(MESSAGE_NO_DESCRIPTION_ARGUMENT, e.getMessage());
+        }
+    }
+
+    @Test 
+    public void parse_validEventWithDateTime_returnsEventWithDateTime() {
+        String input = "event project meeting /from 2024-10-10 14:00 /to 2024-10-10 16:00";
+        Command expected = new AddEventCommand("project meeting", LocalDateTime.of(2024, 10, 10, 14, 0), LocalDateTime.of(2024, 10, 10, 16, 0));
+        Command result = null;
+        try {
+            result = Parser.parse(input);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        assertEquals(expected, result);
+    }
+
+    @Test 
+    public void parse_validEventWithStringTime_returnsEventWithStringTime() {
+        String input = "event project meeting /from monday /to sunday";
+        Command expected = new AddEventCommand("project meeting", "monday", "sunday");
+        Command result = null;
+        try {
+            result = Parser.parse(input);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        assertEquals(expected, result);
+    }
+
+    @Test
+    public void parse_invalidEventNoDescription_throwsException() {
+        String input = "event";
+        Command result = null;
+        try {
+            result = Parser.parse(input);
+        } catch (Exception e) {
+            assertEquals(MESSAGE_WRONG_FROM_KEYWORD, e.getMessage());
+        }
+    }
+
+    @Test
+    public void parse_invalidEventNoFrom_throwsException() {
+        String input = "event project meeting /to 2024-10-10 16:00";
+        Command result = null;
+        try {
+            result = Parser.parse(input);
+        } catch (Exception e) {
+            assertEquals(MESSAGE_WRONG_FROM_KEYWORD, e.getMessage());
+        }
+    }
+
+    @Test
+    public void parse_invalidEventNoTo_throwsException() {
+        String input = "event project meeting /from 2024-10-10 14:00";
+        Command result = null;
+        try {
+            result = Parser.parse(input);
+        } catch (Exception e) {
+            assertEquals(MESSAGE_WRONG_TO_KEYWORD, e.getMessage());
+        }
+    }
+
+    @Test
+    public void parse_invalidEventWrongOrder_throwsException() {
+        String input = "event project meeting /to 2024-10-10 14:00 /from 2024-10-10 16:00";
+        Command result = null;
+        try {
+            result = Parser.parse(input);
+        } catch (Exception e) {
+            assertEquals(MESSAGE_WRONG_TO_KEYWORD, e.getMessage());
+        }
+    }
+
+    @Test
+    public void parse_invalidEventLackArguments_throwsException() {
+        String input = "event /from /to";
+        Command result = null;
+        try {
+            result = Parser.parse(input);
+        } catch (Exception e) {
+            assertEquals(MESSAGE_WRONG_FROM_KEYWORD, e.getMessage());
         }
     }
 
