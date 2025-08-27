fc1cfd2c0d871af833930696bea58bac9e05e775
Zechariah Yeo <70231898+not-a-scam@users.noreply.github.com>
2025-08-27 23:26:18 +0800

    InvalidNomzCommandException.java: Add JavaDoc comments

--- src/main/java/nomz/data/exception/InvalidNomzCommandException.java
+++ src/main/java/nomz/data/exception/InvalidNomzCommandException.java
@@ -1,6 +1,13 @@
 package nomz.data.exception;
+
+/**
+ * Exception thrown for invalid commands in Nomz.
+ */
 public class InvalidNomzCommandException extends NomzException {
 
+    /**
+     * Creates an InvalidNomzCommandException
+     */
     public InvalidNomzCommandException() {
         super("idk that command :(");
     }
