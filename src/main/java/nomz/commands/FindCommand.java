0ff1d8c66448353b51d9f649e9fd6ff64d004484
Zechariah Yeo <70231898+not-a-scam@users.noreply.github.com>
2025-08-28 09:59:33 +0800

    Add FindCommand.java

---
+++ src/main/java/nomz/commands/FindCommand.java
@@ -0,0 +1,48 @@
+package nomz.commands;
+
+import java.util.ArrayList;
+
+import static nomz.common.Messages.MESSAGE_FIND_NO_MATCH;
+import static nomz.common.Messages.MESSAGE_FIND_RESULTS_HEADER;
+import nomz.data.exception.NomzException;
+import nomz.data.tasks.Task;
+import nomz.data.tasks.TaskList;
+import nomz.storage.Storage;
+import nomz.ui.Ui;
+
+/** 
+ * Command to find tasks by description
+ */
+public class FindCommand extends Command{
+    private String keyword;
+
+    /**
+     * Creates a FindCommand to search for tasks by description.
+     * @param keyword the keyword to search for
+     */
+    public FindCommand(String keyword) {
+        this.keyword = keyword;
+    }
+
+    @Override
+    public void execute(TaskList tasks, Ui ui, Storage storage) throws NomzException {
+        ArrayList<Task> matched = new ArrayList<>();
+        for (Task t : tasks.getTasks()) {
+            if (t.getDescription().contains(this.keyword)) {
+                matched.add(t);
+            }
+        }
+
+        if (matched.isEmpty()) {
+            ui.show(MESSAGE_FIND_NO_MATCH.formatted(this.keyword));
+        } else {
+            StringBuilder sb = new StringBuilder();
+            sb.append(MESSAGE_FIND_RESULTS_HEADER);
+            for (int i = 0; i < matched.size(); i++) {
+                sb.append((i + 1) + ". " + matched.get(i).toString() + "\n");
+            }
+            ui.show(sb.toString().trim());
+            
+        }
+    }
+}
