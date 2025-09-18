94f1061993e3e4bbd3e338ef3a66e37613e09391
Zechariah Yeo <70231898+not-a-scam@users.noreply.github.com>
2025-09-18 12:36:18 +0800

    Messages.java: Organise messages

--- src/main/java/nomz/common/Messages.java
+++ src/main/java/nomz/common/Messages.java
@@ -14,33 +14,32 @@ public class Messages {
     // Tasks
     public static final String MESSAGE_ADD_TASK = "Nomz haz added:\n\t%s\nto the nomz list!";
     public static final String MESSAGE_ADD_TAG = "Nomz has added the tag!:\n\t%s";
-    public static final String MESSAGE_TASK_UNMARKED = "Nomz has unmarked your task:\n%s";
-    public static final String MESSAGE_TASK_MARKED = "Nomz says good job!:\n%s";
     public static final String MESSAGE_DELETE_TASK = "nomz haz removed task %s from the nomz list";
+    public static final String MESSAGE_TASK_MARKED = "Nomz says good job!:\n%s";
+    public static final String MESSAGE_TASK_UNMARKED = "Nomz has unmarked your task:\n%s";
 
     // Errors
-    public static final String MESSAGE_INVALID_INTEGER_ARGUMENT = "your index argument is not a valid integer!";
-    public static final String MESSAGE_INVALID_TASK_INDEX = "task index is out of bounds!";
-    public static final String MESSAGE_INVALID_FORMAT = "the file contains an invalid format :(";
+    public static final String MESSAGE_INVALID_COMMAND = "idk that command :(";
+    public static final String MESSAGE_INVALID_DEADLINE =
+        "you used the command wrongly :((\nuse \"deadline <description> /by <due time>\"";
+    public static final String MESSAGE_INVALID_EVENT =
+        "you used the command wrongly :((\nuse \"event <description> /from <start time> /to <end time>\"";
     public static final String MESSAGE_INVALID_FILE_FORMAT =
         "uh oh, there were some errors loading your tasks from file :(\nplease check the file for errors!";
+    public static final String MESSAGE_INVALID_INTEGER_ARGUMENT = "your index argument is not a valid integer!";
+    public static final String MESSAGE_INVALID_FORMAT = "the file contains an invalid format :(";
+    public static final String MESSAGE_INVALID_FROM_KEYWORD = "you didnt use the /from keyword properly :((";
+    public static final String MESSAGE_INVALID_SAVE_STRING = "the file contains an invalid save string :(";
+    public static final String MESSAGE_INVALID_TASK_INDEX = "task index is out of bounds!";
+    public static final String MESSAGE_INVALID_TO_KEYWORD = "you didnt use the /to keyword properly :((";
 
-    public static final String MESSAGE_NO_INDEX_ARGUMENT = "you need to provide an index argument :((";
-    public static final String MESSAGE_NO_DESCRIPTION_ARGUMENT = "you didnt specify the task :((";
     public static final String MESSAGE_NO_ARGUMENTS = "you don't have enough arguments :(";
     public static final String MESSAGE_NO_BY_KEYWORD = "you didnt use the /by keyword :((";
+    public static final String MESSAGE_NO_DESCRIPTION_ARGUMENT = "you didnt specify the task :((";
+    public static final String MESSAGE_NO_INDEX_ARGUMENT = "you need to provide an index argument :((";
     public static final String MESSAGE_NO_TAG_DESCRIPTION = "you need to provide a tag :((";
+
     public static final String MESSAGE_DUPLICATE_KEYWORD = "you used the %s keyword more than once :((";
-
-    public static final String MESSAGE_WRONG_FROM_KEYWORD = "you didnt use the /from keyword properly :((";
-    public static final String MESSAGE_WRONG_TO_KEYWORD = "you didnt use the /to keyword properly :((";
-    public static final String MESSAGE_WRONG_EVENT =
-        "you used the command wrongly :((\nuse \"event <description> /from <start time> /to <end time>\"";
-    public static final String MESSAGE_WRONG_DEADLINE =
-        "you used the command wrongly :((\nuse \"deadline <description> /by <due time>\"";
-
-    public static final String MESSAGE_INVALID_COMMAND = "idk that command :(";
-    public static final String MESSAGE_INVALID_SAVE_STRING = "the file contains an invalid save string :(";
 
     // Find Command Messages
     public static final String MESSAGE_FIND_NO_MATCH = "nomz couldn't find any tasks with: %s";
